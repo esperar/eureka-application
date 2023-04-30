b69887536f13ba041dbb29ec3a2eb984ea4070a7
aesperer <piyrw9754@gmail.com>
2023-04-30 09:46:59 +0900

    setting application eureka

---
+++ src/main/kotlin/esperer/eurekaapplication/EurekaApplication.kt
@@ -0,0 +1,11 @@
+package esperer.eurekaapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaApplication
+
+fun main(args: Array<String>) {
+	runApplication<EurekaApplication>(*args)
+}
---
+++ src/test/kotlin/esperer/eurekaapplication/EurekaApplicationTests.kt
@@ -0,0 +1,13 @@
+package esperer.eurekaapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
